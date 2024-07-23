e458c392d3116be0d7ef1fe2ca8e14cf5441b839
MehdiHasssan <mehdihassan16716@gmail.com>
2024-07-23 13:56:20 +0500

    Features added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,6 +30,9 @@ const clients = [
   ['Green Life', logoGreenLife],
   ['Bright Path', logoBrightPath],
   ['North Adventures', logoNorthAdventures],
+  ['Confidential', logoUnseal],
+  ['Responsive Solutions', logoUnseal],
+  ['Case Summary', logoUnseal], 
 ]
 
 function Clients() {
@@ -82,7 +85,6 @@ function CaseStudies({
             <FadeIn key={caseStudy.href} className="flex">
               <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
                 <h3>
-                  {/* <Link href={caseStudy.href}> */}
                     <span className="absolute inset-0 rounded-3xl" />
                     <Image
                       src={caseStudy.logo}
@@ -90,20 +92,8 @@ function CaseStudies({
                       className="h-16 w-16"
                       unoptimized
                     />
-                  {/* </Link> */}
+
                 </h3>
-                {/* <p className="mt-6 flex gap-x-2 text-sm text-neutral-950">
-                  <time
-                    dateTime={caseStudy.date.split('-')[0]}
-                    className="font-semibold"
-                  >
-                    {caseStudy.date.split('-')[0]}
-                  </time>
-                  <span className="text-neutral-300" aria-hidden="true">
-                    /
-                  </span>
-                  <span>Case study</span>
-                </p> */}
                 <p className="mt-6 font-display text-2xl font-semibold text-neutral-950">
                   {caseStudy.title}
                 </p>
@@ -176,7 +166,7 @@ export const metadata: Metadata = {
 }
 
 export default async function Home() {
-  let caseStudies = (await loadCaseStudies()).slice(0, 3)
+  let caseStudies = (await loadCaseStudies()).slice(0, 6)
 
   return (
     <>
