abf17385a637329cb9a798422c1028e2457818cd
Mohammad Shaheer Gul <mohammadshaheer342@gmail.com>
2024-08-15 19:22:41 +0500

    small changes

--- src/components/CaseSummaryModel.tsx
+++ src/components/CaseSummaryModel.tsx
@@ -12,22 +12,22 @@ function formatResponseText(inputMessage: string): string {
 
   // Replace general headings, removing leading hyphens, numbers, and spaces
   let finalText = inputMessage.replace(generalHeadingPattern, (match, p1, p2) => {
-    return `<h3 style="font-size: 1.5em; margin-top: 10px;"><strong>${p2.trim()}:</strong></h3>`;
+    return `<h3 style="font-size: 1.4em; margin-top: 8px; color: black;"><strong style="font-weight: 600;">${p2.trim()}:</strong></h3>`;
   });
 
   // This is to ensure markdown style headers are also captured and formatted
   finalText = finalText.replace(markdownHeaderPattern, (match, p1) => {
-    return `<h3 style="font-size: 1.5em; margin-top: 10px;"><strong>${p1.trim()}</strong></h3>`;
+    return `<h3 style="font-size: 1.4em; margin-top: 8px; color: black;"><strong style="font-weight: 600;">${p1.trim()}</strong></h3>`;
   });
 
   // Apply bold formatting using HTML <strong> tags for inline bold text marked with **
-  finalText = finalText.replace(boldTextPattern, (match, p1) => `<strong>${p1}</strong>`);
+  finalText = finalText.replace(boldTextPattern, (match, p1) => `<strong style="font-weight: 600; color: black;">${p1}</strong>`);
 
   // Special formatting for "Case Number"
-  finalText = finalText.replace(caseNumberPattern, (match, p1) => `<span style="font-weight: 700; font-size: 1.3em;">Case Number: &#8226; ${p1.trim()}</span>`);
+  finalText = finalText.replace(caseNumberPattern, (match, p1) => `<span style="font-weight: 600; font-size: 1.2em; color: black;">Case Number: &#8226; ${p1.trim()}</span>`);
 
   // Format lines that start with `-`
-  finalText = finalText.replace(/^- (.*)/gm, (match, p1) => `<p>${p1.trim()}</p>`);
+  finalText = finalText.replace(/^- (.*)/gm, (match, p1) => `<p style="color: black;">${p1.trim()}</p>`);
 
   // Clean up any excessive new lines
   finalText = finalText.replace(/\n{2,}/g, "\n\n");
@@ -37,13 +37,14 @@ function formatResponseText(inputMessage: string): string {
 }
 
 
+
 export default function CaseSummaryModal({ isOpen, closeModal, order_num, case_description }: any) {
   const [summary, setSummary] = useState('');
   console.log("hehkasdfajhdfjafdjh", summary)
 
   const fetchCaseSummary = async () => {
     try {
-      const response = await fetch('https://007c-2407-aa80-314-d956-7d13-2348-47ce-f57c.ngrok-free.app/summarize', {
+      const response = await fetch('https://4977-2407-aa80-314-b639-2d00-975c-1d3a-86c1.ngrok-free.app/summarize', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
