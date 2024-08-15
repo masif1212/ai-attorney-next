fb28d07ae4d9a0a6b089fa8e87b137219a67705b
MehdiHasssan <mehdihassan16716@gmail.com>
2024-08-15 12:13:51 +0500

    case summary section is added

--- src/app/casedetail/page.tsx
+++ src/app/casedetail/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
+import { useState } from 'react'
+import CaseSummaryModal from '../../components/CaseSummaryModel' // Ensure this path is correct based on where you store the modal component
 import casesData from '../../../data/casesdata.json'
 
 export default function CaseDetail() {
   const searchParams = useSearchParams()
   const order_num = searchParams?.get('order_num')
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   // Find the case by the order_num from the URL
   const caseItem = casesData.find((item) => item.order_num === order_num)
@@ -14,15 +17,32 @@ export default function CaseDetail() {
     return <p className="text-center text-gray-700">Case not found.</p>
   }
 
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
   return (
     <div className="min-h-screen w-full bg-gradient-to-r from-gray-100 to-gray-300 p-8">
       <div className="mx-auto w-full max-w-6xl">
-        <button
-          onClick={() => window.history.back()}
-          className="mb-4 inline-flex items-center rounded-lg bg-black px-6 py-2 text-lg font-medium text-white shadow-md transition duration-300 ease-in-out hover:bg-gray-700 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-        >
-          Back
-        </button>
+        <div className='flex flex-row justify-between'>
+          <button
+            onClick={() => window.history.back()}
+            className="mb-4 inline-flex items-center rounded-lg bg-black px-6 py-2 text-lg font-medium text-white shadow-md transition duration-300 ease-in-out hover:bg-gray-700 hover:shadow-lg focus:outline-none"
+          >
+            Back
+          </button>
+          <button
+            onClick={openModal}
+            className="mb-4 inline-flex items-center rounded-lg bg-black px-6 py-2 text-lg font-medium text-white shadow-md transition duration-300 ease-in-out hover:bg-gray-700 hover:shadow-lg focus:outline-none"
+          >
+            Case Summary
+          </button>
+        </div>
+
         <div className="rounded-lg bg-white p-8 shadow-xl">
           <h1 className="mb-4 text-center text-2xl font-extrabold text-gray-800">
             {caseItem.title}
@@ -42,6 +62,14 @@ export default function CaseDetail() {
           {/* You can add more case details here */}
         </div>
       </div>
+
+      {/* Modal Component */}
+      <CaseSummaryModal
+        isOpen={isModalOpen}
+        closeModal={closeModal}
+        order_num={caseItem.order_num}
+        case_description={caseItem.case_description}
+      />
     </div>
   )
 }
