0d77d8a79f6158e65e16091aaf4fd2fbe4b0a61b
muhammad_muzammil.dev <muzammil.dev46@gmail.com>
2024-08-19 18:13:48 +0500

    push

---
+++ server.js
@@ -0,0 +1,41 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+const hostname = '0.0.0.0'; // Bind to all network interfaces
+
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
+
+      // Add a check to bypass custom routing for API routes
+      if (pathname.startsWith('/api')) {
+        await handle(req, res, parsedUrl); // Let Next.js handle the API routes
+      } else if (pathname === '/a') {
+        await app.render(req, res, '/a', query);
+      } else if (pathname === '/b') {
+        await app.render(req, res, '/b', query);
+      } else {
+        await handle(req, res, parsedUrl);
+      }
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('internal server error');
+    }
+  })
+    .once('error', (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, hostname, () => {
+        console.log(`Ready on http://${hostname}:${port} `)
+        });
+});
