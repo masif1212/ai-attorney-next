4164c25e0f10421727eaddc56c3934958eee998a
muhammad_muzammil.dev <muzammil.dev46@gmail.com>
2024-08-19 15:52:48 +0500

    test

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -117,8 +117,8 @@ function Header({
         <div className="flex items-center gap-x-8">
 
           {
-            pathname !== '/signin' && (
-              <Button href="/signin" invert={invert}>
+            pathname !== '/Signin' && (
+              <Button href="/Signin" invert={invert}>
                 Sign in
               </Button>
             )
