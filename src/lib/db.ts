35987a729845345c6cbea8389a40eabed4971ce0
masif1212 <asifturi1299@gmail.com>
2024-08-28 17:23:20 +0500

    db changes

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,24 +1,23 @@
 // src/lib/db.js
-import mysql, { Connection } from 'mysql2/promise';
+import mysql, { Connection } from 'mysql2/promise'
 
 // mysql://root:gYvwbsccsHHJDEzcPLMznQXbeYQDNvPW@junction.proxy.rlwy.net:55944/railway
 const connectToDatabase = async (): Promise<Connection | null> => {
   try {
     const connection = await mysql.createConnection({
-      host: 'localhost',
+      host: process.env.DATABASE_HOST,
       port: 3306,
-      user: 'root',
-      password: 'root123',
-      database: 'aiattorney',
-    });
-  
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+    })
 
-    console.log("Database connected Successfully")
-    return connection;
+    console.log('Database connected Successfully')
+    return connection
   } catch (error) {
-    console.error('Error connecting to the database:', error);
-    return null;
+    console.error('Error connecting to the database:', error)
+    return null
   }
-};
+}
 
-export default connectToDatabase;
+export default connectToDatabase
